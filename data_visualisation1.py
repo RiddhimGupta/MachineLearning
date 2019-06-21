c673e40781f64b5d7144e65c0efbfaf7a258c219
Riddhim Gupta <gupta.riddhim1@gmail.com>
2019-06-21 22:43:12 +0530

    Update data_visualisation1.py

--- data_visualisation1.py
+++ data_visualisation1.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import pandas as pd
 import matplotlib.pyplot as plt
-#reading file
+#reading file student
 sc = pd.read_csv('student.csv') 
 #plotting pie-chart
 plt.pie(sc['Marks'],labels=sc['Student_Name'],shadow=True,autopct='%1.1f%%',startangle=90)

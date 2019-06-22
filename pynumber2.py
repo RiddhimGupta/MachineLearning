b10e42ce527541c5670d2dc09cc96c4869d75f6a
Riddhim Gupta <gupta.riddhim1@gmail.com>
2019-06-22 13:52:24 +0530

    Update pynumber2.py

--- pynumber2.py
+++ pynumber2.py
@@ -2,4 +2,5 @@
 import numpy as np
 #arange*(start,stop,step)[values to be included].reshape(rows,columns)
 arr=np.arange(100,200,5)[0:16].reshape(8,2)
+#printing the newly generated array
 print(arr)
